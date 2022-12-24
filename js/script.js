dc0b3e2317f334ffd6cd10e9fd5597ae79822926
louiskgkim <lkgk31@gmail.com>
2022-12-23 21:07:41 -0800

    fixed up all the code in order to pull the API for the excercises from the dropdown menu

--- js/script.js
+++ js/script.js
@@ -10,18 +10,18 @@ let date = dayjs().format("dddd, MMMM DD YYYY");
 let citySearch = [];
 
 // Save text value of search into an array and storage
-$("#weather-search").on("click", function(event){
-  event.preventDefault();
+$("#weather-search").on("click", function (event) {
+	event.preventDefault();
 	// Had to relook into "this" https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this
-  city = $(this).parent(".btnPar").siblings(".textVal").val();
-  if (city === "") {
-    return;
-  };
-  citySearch.push(city);
+	city = $(this).parent(".btnPar").siblings(".textVal").val();
+	if (city === "") {
+		return;
+	};
+	citySearch.push(city);
 
 	// set local storage for city's that were searched
-  localStorage.setItem("city", JSON.stringify(citySearch));
-  getFiveDayForecastEl.empty();
+	localStorage.setItem("city", JSON.stringify(citySearch));
+	getFiveDayForecastEl.empty();
 	// getHistory();
 	getTodayWeather();
 });
@@ -31,29 +31,29 @@ let cardContent = $(".cardContent");
 // Apply weather data to today's card and launch 5 day forecast
 function getTodayWeather() {
 
-  // API provided for challenge
-  // https://openweathermap.org/forecast5
-  let getUrlCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`;
-  
-  $(cardContent).empty();
+	// API provided for challenge
+	// https://openweathermap.org/forecast5
+	let getUrlCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`;
+
+	$(cardContent).empty();
 
 	$.ajax({
 		url: getUrlCurrent,
 		method: "GET",
-  }).then(function (response) {
-    $(".cardTodaysCityName").text(response.name);
-    $(".cardTodaysDate").text(date);
+	}).then(function (response) {
+		$(".cardTodaysCityName").text(response.name);
+		$(".cardTodaysDate").text(date);
 
-    //Icons: https://openweathermap.org/weather-conditions
+		//Icons: https://openweathermap.org/weather-conditions
 		$(".icons").attr("src", `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`);
 		// Temperature
 		let pEl = $("<p>").text(`Temperature: ${response.main.temp} °F`);
 		cardContent.append(pEl);
 		// Lat and long of searched city
-    let cityLat = response.coord.lat;
+		let cityLat = response.coord.lat;
 		let cityLon = response.coord.lon;
-    
-		});
+
+	});
 	getFiveDayForecast();
 };
 
@@ -63,34 +63,34 @@ let getFiveDayForecastEl = $(".fiveForecast");
 function getFiveDayForecast() {
 	let getUrlFiveDay = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${key}`;
 
-  $.ajax({
+	$.ajax({
 		url: getUrlFiveDay,
 		method: "GET",
 	}).then(function (response) {
 		let fiveDayArray = response.list;
 		let weatherCards = [];
 
-  // Friend helped me create this array to show data on cards
-  $.each(fiveDayArray, function (index, value) {
-		testObj = {
-			date: value.dt_txt.split(" ")[0],
-			time: value.dt_txt.split(" ")[1],
-			temp: value.main.temp,
-			icon: value.weather[0].icon,
-		};
+		// Friend helped me create this array to show data on cards
+		$.each(fiveDayArray, function (index, value) {
+			testObj = {
+				date: value.dt_txt.split(" ")[0],
+				time: value.dt_txt.split(" ")[1],
+				temp: value.main.temp,
+				icon: value.weather[0].icon,
+			};
 
-		// learned helpful tip in splitting again in 12/12/22's class
-		if (value.dt_txt.split(" ")[1] === "12:00:00") {
-			weatherCards.push(testObj);
-		}
-		});  
-	
+			// learned helpful tip in splitting again in 12/12/22's class
+			if (value.dt_txt.split(" ")[1] === "12:00:00") {
+				weatherCards.push(testObj);
+			}
+		});
+
 		//Cards to display on screen
 		for (let i = 0; i < weatherCards.length; i++) {
 
 			let divElCard = $("<div>");
 			divElCard.attr("class", "card text-black bg-light mb-3 cardOne");
-			divElCard.attr("style", "max-width: 800px;");
+			divElCard.attr("style", "max-width: 1000px;");
 			getFiveDayForecastEl.append(divElCard);
 
 			let divElHeader = $("<div>");
@@ -127,102 +127,64 @@ function initLoad() {
 	getTodayWeather();
 };
 
-// muscle starts here
+// MUSCLE STARTS HERE
+let muscle = "biceps";
+
+$("#muscleOptions").on("change", function (event) {
+	event.preventDefault();
+	console.log('button clicked');
+	console.log($(this).val());
+	muscle = $(this).val();
+
+	// https://materializecss.com/select.html 
+
+	// muscle = $(this).parent(".btnBod").siblings(".textBod").val();
+	if (muscle === "") {
+		return;
+	};
+
+	// localStorage.setItem("muscle", JSON.stringify(muscleOptions));
+});
+
+$("#muscle-search").on("click", function (event) {
+	event.preventDefault();
+	getTodayMuscle();
+});
+
+let muscleCont = $(".muscleContent");
+// name, muscle, equipment
+let muscleInst = $(".muscleInstructions");
+// instructions
 
 // Received API GET query from https://api-ninjas.com/api/exercises
 function getTodayMuscle() {
-	let cardContent = $(".cardContent");
-	let getUrlMuscle = `https://api.api-ninjas.com/v1/exercises?muscle=`;
-	
-	$(cardContent).empty();
+	let getUrlMuscle = `https://api.api-ninjas.com/v1/exercises?muscle=` + muscle;
 
-// let key2 = "2V+JOSM/L4NDwhAccQy2NQ==Ff6aU29r1pMP8WuX";
-let muscle = [biceps];
-
-$.ajax({
-    method: 'GET',
+	$.ajax({
+		method: 'GET',
 		// need to work on template literals https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
-    url: `https://api.api-ninjas.com/v1/exercises?muscle=${muscle}` + muscle,
-    headers: { 'X-Api-Key': '2V+JOSM/L4NDwhAccQy2NQ==Ff6aU29r1pMP8WuX'},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-});
-
-let muscleSearch = [];
-
-$("#muscle-search").on("click", function(event){
-  event.preventDefault();
-  console.log('button clicked')
-	
-  muscle = $(this).parent(".btnBod").siblings(".textBod").val();
-  if (muscle === "") {
-    return;
-  };
-  muscleSearch.push(muscle);
-
-  localStorage.setItem("muscle", JSON.stringify(muscleSearch));
-});
-};
-
-// CALL OUT BUTTON FOR WORKOUTS
-
-let fiveMuscleListEl = $(".fiveMuscle");
-
-	function getFiveMuscleList() {
-		let cardContent = $(".cardContent");
-		// need to work on template literals https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
-	  let getUrlMuscle = `https://api.api-ninjas.com/v1/exercises?muscle=back`;
-	  
-	  $(cardContent).empty();
-	
-		$.ajax({
-			url: getUrlMuscle,
-			method: "GET",
-	  }).then(function (response) {
-		$(".muscleGroup").text(response.muscle);
-		$(".muscleContent").text(response.name);
-		$(".muscleInstructions").text(response.instructions);
-	
-		getFiveMuscleList();
-	});
-
-  // we need to fix this
-  $.each(fiveDayMuscle, function (index, value) {
-		testObj = {
-			muscle: value.dt_txt.split(" ")[0],
-			name: value.dt_txt.split(" ")[1],
-			instructions: value.dt_txt.split(" ")[2],
-
-		};
-
-		if (value.dt_txt.split(" ")[1] === "muscle") {
-			muscleCards.push(testObj);
+		url: getUrlMuscle,
+		headers: { 'X-Api-Key': '2V+JOSM/L4NDwhAccQy2NQ==Ff6aU29r1pMP8WuX' },
+		contentType: 'application/json',
+		success: function (response) {
+			console.log(response[0].name);
+		},
+		error: function ajaxError(jqXHR) {
+			console.error('Error: ', jqXHR.responseText);
 		}
+	}).then(function (response) {
+		response.forEach(workout => {
+			console.log(workout.name);
+			let pElOne = $("<p>").text(`Name: ${workout.name}`);
+			let pElTwo = $("<p>").text(`Muscle: ${workout.muscle}`);
+			let pElThree = $("<p>").text(`Equipment: ${workout.equipment}`);
+			muscleCont.append(pElOne);
+			muscleCont.append(pElTwo);
+			muscleCont.append(pElThree);
+			let pElFour = $("<p>").text(`Instructions: ${workout.instructions}`);
+			muscleCont.append(pElFour);
 		});
-	
-		//Cards to display on screen
-		for (let i = 0; i < muscleCards.length; i++) {
-
-			let divElCard = $("<div>");
-			divElCard.attr("class", "card text-black bg-light mb-3 cardThree");
-			divElCard.attr("style", "max-width: 800px;");
-			getFiveMuscleListEl.append(divElCard);
-
-			let divElBody = $("<div>");
-			divElBody.attr("class", "card-body2");
-			divElCard.append(divElBody);
-
-		}
+	});
 };
 
 initLoad();
-
-// test
-// test
-// test
-// test
